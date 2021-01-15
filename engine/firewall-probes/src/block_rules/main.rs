fc9bbd448bbeaf4c8c1dff51d368e1ab8cb5081a
Dunateo <bruvalentin84@icloud.com>
2021-01-15 15:52:01 +0100

    fin test

--- engine/firewall-probes/src/block_rules/main.rs
+++ engine/firewall-probes/src/block_rules/main.rs
@@ -1,14 +1,15 @@
-#![no_std]
+r#![no_std]
 #![no_main]
 
 use redbpf_probes::tc::*;
+use std::ops::Try;
 
 program!(0xFFFFFFFE, "GPL");
 
 #[tc_action]
 pub fn block_port_80(ctx: TcActionResult) -> TcActionResult {
-    if let Ok(transport) = ctx.transport() {
-        if transport.dest() == 80 {
+    if let Ok(transport) = ctx.into(){
+        if transport. == 80 {
             return Ok(TcAction::Shot);
             
         }else if transport.source() == 80  {
